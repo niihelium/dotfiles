5b5871dac2471982088f140749906171b48814bf
niihelium <nii.ic434@gmail.com>
2015-05-05 19:51:18 +0300

    Added

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ mv .bashrc ~/dot_backup/
 ln -s ~/.dotfiles/.bashrc ./.bashrc
 ln -s ~/.dotfiles/.vimrc ./.vimrc
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
+
 
