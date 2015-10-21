090347e2a18d928fb2b70e8f75740e4cec574189
Nikita <nii.ic434@gmail.com>
2015-10-21 11:16:31 +0300

    Some

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
-mkdir ~/dot_backup
+mkdir ~/.dotfiles/dot_backup
 
-mv .vimrc ~/dot_backup/
-mv .bashrc ~/dot_backup/
+ln -s ~/.dotfiles/bin ~/bin
 
-ln -s ~/.dotfiles/.bashrc ./.bashrc
-ln -s ~/.dotfiles/.vimrc ./.vimrc
+sudo chmod -R +x ~/.dotfiles
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall
+#mv .vimrc ~/dot_backup/
+#mv .bashrc ~/dot_backup/
+
+#ln -s ~/.dotfiles/.bashrc ./.bashrc
+#ln -s ~/.dotfiles/.vimrc ./.vimrc
+
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/#Vundle.vim
+#vim +PluginInstall
 
 #hello
