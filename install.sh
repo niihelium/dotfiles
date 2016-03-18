251d42f0abc1d883cd015a61b0fd8f93ce12d7ce
Nikita Unkovsky <nii.unkovsky@gmail.com>
2016-03-18 21:59:49 +0300

    add ~/bin to PATH

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ sudo apt-get install i3 vim xcompmgr feh
 
 mkdir ~/.dotfiles/dot_backup
 
+echo "# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ]; then
+  PATH="$HOME/bin:$PATH"
+fi" >> ~/.profile
+
 ln -s ~/.dotfiles/bin ~/bin
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.profile ~/.profile
